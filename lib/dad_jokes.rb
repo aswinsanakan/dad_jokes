da3f88f2bcc9ea2d3340857b0aa02668895cea22
aswinsanakan <aswinsanakan@gmail.com>
2017-10-27 02:08:10 +0530

    Add search for jokes

--- lib/dad_jokes.rb
+++ lib/dad_jokes.rb
@@ -6,26 +6,49 @@ require 'pry'
 
 module DadJokes
 
-  API_URL = "https://icanhazdadjoke.com/"
+  API_URL = "https://icanhazdadjoke.com/".freeze
   API_HEADERS = {"Accept": "application/json"}
+  DEFAULT_LIMIT = 5
 
   def self.random
     uri = "/"
-    get_joke(uri)
+    joke_object = parse_response(uri)
+    get_joke(joke_object)
   end
 
   def self.fetch(joke_id:)
     uri = "/j/#{joke_id}"
-    get_joke(uri)
+    joke_object = parse_response(uri)
+    get_joke(joke_object)
+  end
+
+  def self.search(term: '',limit: DEFAULT_LIMIT)
+    uri = "/search?term=#{term}&limit=#{limit}"
+    response = connection.get(uri)
+    jokes_hash = JSON.parse(response.body)
+    jokes = filter_jokes(jokes_hash)
+    jokes
   end
 
   private
 
-  def self.get_joke(uri)
+  def self.parse_response(uri)
     response = connection.get(uri)
-    parsed_response = parse_response(response.body)
-    joke = parsed_response['joke']
-    joke
+    JSON.parse(response.body)
+  end
+
+  def self.get_joke(joke_object)
+    joke_object['joke']
+  end
+
+  def self.filter_jokes(jokes_hash)
+    # binding.pry
+    jokes_arr = jokes_hash['results']
+    jokes = []
+    jokes_arr.each do |joke|
+      jokes << joke['joke']
+    end
+    jokes
   end
   
   def self.connection
@@ -33,7 +56,7 @@ module DadJokes
   end
 
   def self.parse_response(body)
-    JSON.parse(body)
+    
   end
 
 end
