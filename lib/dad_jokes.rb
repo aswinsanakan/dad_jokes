96b1dfb5f580c05d9400ca3187048c1957988f45
aswinsanakan <aswinsanakan@gmail.com>
2017-11-02 17:22:50 +0530

    Add feature : generate random joke as image

--- lib/dad_jokes.rb
+++ lib/dad_jokes.rb
@@ -2,11 +2,11 @@ require "dad_jokes/version"
 require "faraday"
 require "faraday_middleware"
 require "json"
-require 'pry'
+require 'open-uri'
 
 module DadJokes
 
-  API_URL = "https://icanhazdadjoke.com/".freeze
+  API_URL = "https://icanhazdadjoke.com".freeze
   API_HEADERS = {"Accept": "application/json"}
   DEFAULT_LIMIT = 5
 
@@ -14,6 +14,18 @@ module DadJokes
     uri = "/"
     joke_object = parse_response(uri)
     get_joke(joke_object)
+  end
+
+  def self.random_image
+    uri = "/"
+    joke_object = parse_response(uri)
+    joke_id = get_id(joke_object)
+    image_uri = "/j/#{joke_id}.png"
+    url = File.join(API_URL, image_uri).to_s
+    download = open(url)
+    path = get_image_save_path(joke_id)
+    IO.copy_stream(download, path)
+    "#{get_joke(joke_object)} ( Saved as image in location: #{path} !)"
   end
 
   def self.fetch(joke_id:)
@@ -41,9 +53,12 @@ module DadJokes
     joke_object['joke']
   end
 
+  def self.get_id(joke_object)
+    joke_object['id']
+  end
+
   def self.filter_jokes(jokes_hash)
-    # binding.pry
-    jokes_arr = jokes_hash['results']
+    jokes_arr = jokes_hash['results'] || []
     jokes = []
     jokes_arr.each do |joke|
       jokes << joke['joke']
@@ -55,8 +70,19 @@ module DadJokes
     Faraday.new(url: API_URL, headers: API_HEADERS)
   end
 
-  def self.parse_response(body)
-    
+  def self.get_image_save_path(joke_id)
+    if in_rails_app?
+      File.join(Rails.root,"public","dadjokes","joke_#{joke_id}.png")
+    else
+      "joke_#{joke_id}.png"
+    end 
+  end
+
+  def self.in_rails_app?
+    klass = Module.const_get("Rails")
+    return klass.is_a?(Module)
+  rescue NameError
+    return false
   end
 
 end
